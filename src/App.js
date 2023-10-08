cf67ada508c5ebf05f11d97a8c41d8ef28ea1394
merveonln <merveonalan2@gmail.com>
2023-10-08 21:40:42 +0300

    update invoices page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 // import Form from "./scenes/form";
 // import Calendar from "./scenes/calendar";
 // import FAQ from "./scenes/faq";
@@ -32,7 +32,7 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/calendar" element={<Calendar />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
