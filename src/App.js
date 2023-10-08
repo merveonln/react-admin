009a1c060ebd38285de01fe7145e8476e605ba99
merveonln <merveonalan2@gmail.com>
2023-10-08 21:41:55 +0300

    update form page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
-// import Form from "./scenes/form";
+import Form from "./scenes/form";
 // import Calendar from "./scenes/calendar";
 // import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
@@ -33,7 +33,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/calendar" element={<Calendar />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
