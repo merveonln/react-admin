e2cc5cb07022e04596b33fcd07dbb740793c96b9
merveonln <merveonalan2@gmail.com>
2023-10-08 21:39:01 +0300

    update contact page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
 // import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
 // import Form from "./scenes/form";
 // import Calendar from "./scenes/calendar";
 // import FAQ from "./scenes/faq";
@@ -31,7 +31,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              <Route path="/contacts" element={<Contacts />} />
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/calendar" element={<Calendar />} /> */}
