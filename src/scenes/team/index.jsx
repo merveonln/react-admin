9f85655e2c7485ab3db51abf9ac02b9ca494b0eb
Merve Önalan <92742304+merveonln@users.noreply.github.com>
2024-12-13 17:12:12 +0300

    Update index.jsx

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -5,8 +5,6 @@ import Header from "../../components/Header";
 import { DataGrid } from "@mui/x-data-grid";
 // import { mockDataTeam } from "../../data/mockData";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import axios from "axios";
 
 const Team = () => {
